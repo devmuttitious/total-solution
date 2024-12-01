a45e0a53871b63dbaa98f04ffeda0ffa6ec3db73
devmuttitious <135977801+devmuttitious@users.noreply.github.com>
2024-12-02 02:45:50 +0500

    Update index.js
    Updated

--- index.js
+++ index.js
@@ -27,10 +27,51 @@ app.use(express.json());
 
 // Database Connection
 connectDB();
+// Serve static files from the project root (e.g., index.html, global assets)
+app.use(express.static(path.join(__dirname, '../')));
 
-// Serve static files
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use(express.static(path.join(__dirname, "..")));
+// Serve JavaScript files and plugins
+app.use('/js', express.static(path.join(__dirname, '../js')));
+app.use('/js/plugins', express.static(path.join(__dirname, '../js/plugins')));
+
+// Serve CSS files and plugins
+app.use('/css', express.static(path.join(__dirname, '../css')));
+app.use('/css/plugins', express.static(path.join(__dirname, '../css/plugins')));
+app.use('/scss', express.static(path.join(__dirname, '../scss')));
+
+
+// Serve image files
+app.use('/img', express.static(path.join(__dirname, '../img')));
+app.use('/videos', express.static(path.join(__dirname, '../videos')));
+
+
+// Serve uploads folder
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Serve car-trade and projects folders
+app.use('/car-trade', express.static(path.join(__dirname, '../car-trade')));
+app.use('/projects', express.static(path.join(__dirname, '../projects')));
+
+// Social media redirection routes
+app.get('/social/facebook', (req, res) => {
+    res.redirect('https://web.facebook.com/tst.601?mibextid=ZbWKwL&_rdc=1&_rdr');
+});
+
+app.get('/social/twitter', (req, res) => {
+    res.redirect('https://x.com/search?q=tst_601&t=hSTqqeMPt4IvJZApqT93RA&s=09');
+});
+
+app.get('/social/instagram', (req, res) => {
+    res.redirect('https://www.instagram.com/tst.601/?igshid=YmMyMTA2M2Y');
+});
+
+app.get('/social/whatsapp', (req, res) => {
+    res.redirect('https://api.whatsapp.com/send/?phone=966598301301&text&type=phone_number&app_absent=0');
+});
+
+app.get('/social/snapchat', (req, res) => {
+    res.redirect('https://www.snapchat.com/add/tst.601');
+});
 
 // API Routes
 app.use("/api", subscriberRoutes);
