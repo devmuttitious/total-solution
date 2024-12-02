b243166673fb0e1b6e438c2009c512c774e85ea5
devmuttitious <135977801+devmuttitious@users.noreply.github.com>
2024-12-03 02:10:35 +0500

    Update careerRoutes.js

--- routes/careerRoutes.js
+++ routes/careerRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const upload = require('../middlewares/multer'); // Import multer middleware
 const careerController = require('../controllers/careerController');
 
-// Career form submission route
+
 // Upload multiple files with the names 'resume' and 'coverLetter'
 router.post('/submit', upload.fields([{ name: 'resume', maxCount: 1 }, { name: 'coverLetter', maxCount: 1 }]), careerController.submitCareerForm);
 
