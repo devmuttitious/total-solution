3eb16dbb46cf77ef904cd2954fc1aef74310b530
devmuttitious <135977801+devmuttitious@users.noreply.github.com>
2024-12-02 22:18:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const mediaRoutes = require("./routes/mediaRoute");
 const contactRoutes = require("./routes/contactRoutes");
 const careerRoutes = require("./routes/careerRoutes");
 const fs = require("fs");
-const bodyParser = require("body-parser");
 
 dotenv.config();
 
@@ -17,37 +16,33 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: ["https://tst.com.sa", "http://localhost:3000", "https://total-solution.vercel.app",], // Add localhost for testing
+    origin: ["https://tst.com.sa", "http://localhost:3000", "https://total-solution.vercel.app", "http://127.0.0.1:5502/"], // Add localhost for testing
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
 
 app.use(express.json());
-// Middleware
-app.use(bodyParser.json({ limit: "20mb" })); // Handle large Base64 strings
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // Hey i updated
 
 // Database Connection
 connectDB();
+
 // Serve static files from the project root (e.g., index.html, global assets)
 app.use(express.static(path.join(__dirname, '../')));
 
 // Serve JavaScript files and plugins
 app.use('/js', express.static(path.join(__dirname, '../js')));
+app.use('/scss', express.static(path.join(__dirname, '../scss')));
 app.use('/js/plugins', express.static(path.join(__dirname, '../js/plugins')));
 
 // Serve CSS files and plugins
 app.use('/css', express.static(path.join(__dirname, '../css')));
 app.use('/css/plugins', express.static(path.join(__dirname, '../css/plugins')));
-app.use('/scss', express.static(path.join(__dirname, '../scss')));
-
 
 // Serve image files
 app.use('/img', express.static(path.join(__dirname, '../img')));
 app.use('/videos', express.static(path.join(__dirname, '../videos')));
-
 
 // Serve uploads folder
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
@@ -83,31 +78,17 @@ app.use("/api", mediaRoutes);
 app.use("/api", contactRoutes);
 app.use("/api/career", careerRoutes);
 
-// Serve dynamic static HTML files
-app.get("/:folder?/:page", (req, res) => {
-    const { folder, page } = req.params;
-    const targetFolder = folder && (folder === "projects" || folder === "car-trade") ? folder : "";
-    const filePath = targetFolder
-        ? path.join(__dirname, "..", targetFolder, `${page}.html`)
-        : path.join(__dirname, "..", `${page}.html`);
 
-    console.log("Requested file:", filePath); // Debugging log
-
-    if (fs.existsSync(filePath)) {
-        res.sendFile(filePath);
-    } else {
-        console.error("File not found:", filePath); // Debugging log
-        res.status(404).send("Page not found");
-    }
-});
 
 // Root route to show API status
-app.get("/", (req, res) => {
-    res.send("Your API is working");    
-});
+// app.get("/", (req, res) => {
+//     res.send("Your API is working");    
+// });
 
 // Start the Server
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running at http://localhost:${PORT}`);
     console.log(`Accessible at https://tst.com.sa`);
 });
+
+
